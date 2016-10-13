bd6ce3f6990f06b03937baa63f1a9db072219c2d
OSmol <Olga_Smolyakova@outlook.com>
2016-10-13 10:50:42 +0300

    Update Document.java

--- MyDOM/src/by/epam/training/MyDOMParser/Document.java
+++ MyDOM/src/by/epam/training/MyDOMParser/Document.java
@@ -6,13 +6,15 @@ package by.epam.training.MyDOMParser;
 public class Document {
     private static  Document INSTANCE;
     private Document(){};
-    static Document getDocument() {
+    static Document getDocument() {// у метода потерян атрибут доступа
+        // и зачем этот класс делать singleton?
+        // ты что, парсить единственный файл собираешься за всю работу программы, и два документа сразу даже понадобится не могут?
         if (INSTANCE == null) {
             INSTANCE = new Document();
         }
         return INSTANCE;
     }
-    private Element rootElement;
+    private Element rootElement;// почитай code convention, и не путай где и что располагать
     private String header;
 
     public String getHeader() {return header;}
